fcf524d897a6d78be2865b9740fc444efefa0871
gram <mail@orsinium.dev>
2021-12-31 09:54:21 +0100

    count likes in buttons

--- telegram_stat/_core.py
+++ telegram_stat/_core.py
@@ -1,6 +1,7 @@
+from itertools import chain
 import telethon
 from telethon.tl.patched import Message
-from typing import Any, Dict, Iterator, NamedTuple
+from typing import Any, Dict, Iterator, NamedTuple, Tuple
 
 
 class Channel(NamedTuple):
@@ -16,12 +17,25 @@ class Channel(NamedTuple):
 class Post(NamedTuple):
     message: Message
 
+    @property
+    def buttons(self) -> Iterator[Tuple[str, int]]:
+        buttons = self.message.buttons
+        if not buttons:
+            return
+        for button in chain(*buttons):
+            reaction, _, count = button.button.text.partition(' ')
+            if count.isdigit():
+                yield reaction, int(count)
+            else:
+                yield button.button.text, 0
+
     def as_dict(self) -> Dict[str, Any]:
         m = self.message
         return dict(
             id=m.id,
             date=m.date.isoformat(),
             text=m.message,
-            views=m.views,
-            forwards=m.forwards,
+            views=m.views or 0,
+            forwards=m.forwards or 0,
+            buttons=dict(self.buttons),
         )
